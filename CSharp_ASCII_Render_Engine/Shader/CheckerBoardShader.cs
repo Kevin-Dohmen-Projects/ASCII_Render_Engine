0ee978e3e01a3cd919c6d42b481b2aa6face1c28
Kevin Dohmen <kevin.dohmen@twintos.com>
2024-10-29 17:14:12 +0100

    added some shaders

---
+++ CSharp_ASCII_Render_Engine/Shader/CheckerBoardShader.cs
@@ -0,0 +1,27 @@
+﻿using CSharp_ASCII_Render_Engine.Types.Pixels;
+using CSharp_ASCII_Render_Engine.Types.Vectors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_ASCII_Render_Engine.Shader
+{
+    public class CheckerboardShader : IShader
+    {
+        public Vec2 Render(ShaderPixel shaderPixel)
+        {
+            double col = 0;
+            Vec2 uv = shaderPixel.UV;
+            double frame = (double)shaderPixel.Frame;
+
+            // Checkerboard pattern using the floor of uv coordinates
+            col = ((Math.Floor(uv.x * 10) + Math.Floor(uv.y * 10)) % 2 == 0) ? 1.0 : 0.0;
+
+            shaderPixel.Col.x = col; // luminance
+            shaderPixel.Col.y = 1;   // transparency
+            return shaderPixel.Col;
+        }
+    }
+}
---
+++ CSharp_ASCII_Render_Engine/Shader/ModShader.cs
@@ -0,0 +1,23 @@
+﻿using CSharp_ASCII_Render_Engine.Types.Pixels;
+using CSharp_ASCII_Render_Engine.Types.Vectors;
+
+namespace CSharp_ASCII_Render_Engine.Shader
+{
+    public class ModShader : IShader
+    {
+        public Vec2 Render(ShaderPixel shaderPixel)
+        {
+            double col;
+            Vec2 uv = shaderPixel.UV;
+            double frame = (double)shaderPixel.Frame;
+
+            col = ((uv.x+frame/50)%0.25)*4;
+
+
+            shaderPixel.Col.x = col;
+            shaderPixel.Col.y = 1;
+
+            return shaderPixel.Col;
+        }
+    }
+}
