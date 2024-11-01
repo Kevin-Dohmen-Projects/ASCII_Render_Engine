db7cf333466154863f9aeb35c4a17a4429179da5
Kevin Dohmen <kevin.dohmen@twintos.com>
2024-11-01 10:32:37 +0100

    removed unused using statements + added a source to shaders i didn't write

--- CSharp_ASCII_Render_Engine/Shader/SpiralShader.cs
+++ CSharp_ASCII_Render_Engine/Shader/SpiralShader.cs
@@ -1,15 +1,11 @@
 ﻿using CSharp_ASCII_Render_Engine.Types.Pixels;
 using CSharp_ASCII_Render_Engine.Types.Vectors;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharp_ASCII_Render_Engine.Shader
 {
     public class SpiralShader : IShader
     {
+        // Source: ChatGPT
         public Vec2 Render(ShaderPixel shaderPixel)
         {
             double col = 0;
