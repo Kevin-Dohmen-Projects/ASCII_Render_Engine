c70dde639dcd33af1f7f34d53f9dc1e1c977541c
Kevin Dohmen <kevin.dohmen@twintos.com>
2024-11-14 16:34:27 +0100

    basic template game engine (gona rewrite whole thing (maybe))

---
+++ Example Game Engine/Example_ASCII_Game_Engine/GameObjects/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASCII_Render_Engine.Geometry.Primitives;
+using ASCII_Render_Engine.Types.Vectors;
+using ASCII_Render_Engine.Shader;
+using ASCII_Render_Engine.ScreenRelated;
+using ASCII_Render_Engine.Types.Transform;
+
+namespace Example_ASCII_Game_Engine.GameObjects
+{
+    internal class Ball
+    {
+        public Vec2 Pos;
+        private Circle BallObject;
+        public double Radius;
+
+        public Ball(Vec2 pos)
+        {
+            BallObject = new Circle(new Vec2(0, 0), new Vec2(10, 10), new Vec2(1, 1));
+            Radius = 1;
+            Pos = pos;
+        }
+
+        public Ball(Vec2 pos, IShader shader)
+        {
+            BallObject = new Circle(new Vec2(0, 0), new Vec2(10, 10), shader);
+            Radius = 1;
+            Pos = pos;
+        }
+
+        public IRenderable ToRenderable()
+        {
+            BallObject.Pos.SetInPlace(Pos.x - Radius, Pos.y - Radius);
+            BallObject.Size.SetInPlace(Radius*2, Radius*2);
+            return BallObject;
+        }
+    }
+}
--- Example Game Engine/Example_ASCII_Game_Engine/Program.cs
+++ Example Game Engine/Example_ASCII_Game_Engine/Program.cs
@@ -6,22 +6,26 @@ using ASCII_Render_Engine.Types.Vectors;
 using ASCII_Render_Engine.Utils;
 using ASCII_Render_Engine.Demo.Animation;
 using System.Globalization;
+using Example_ASCII_Game_Engine.GameObjects;
 
 namespace ASCII_Render_Engine
 {
     public static class Program
     {
-        public static Screen screen = new(150, 150);
+        public static Screen screen = new(200, 100);
 
         public static void Main(string[] args)
         {
-            screen.Background = new FullScreenShaderObject(new SinShader());
+            screen.Background = new FullScreenShaderObject(new SpiralShader(), 0.25);
 
             // screen config
             screen.Config.Dithering = true;
-            screen.Config.FPSCap = 20;
+            screen.Config.FPSCap = 30;
             screen.Config.ScaleToWindow = true;
             screen.Config.VisualizeAsync = true;
+
+            //Console.SetWindowSize(screen.Width * 2, screen.Height);
+            //Console.d
 
             Rectangle frame = new Rectangle(new Vec2(0), new Vec2(screen.Width, screen.Height), new Vec2(1, 1), false);
 
@@ -32,9 +36,32 @@ namespace ASCII_Render_Engine
             DateTime frameStartTime = new();
             DateTime frameEndTime = new();
 
+            // game
+            double deltaTime = 0;
+            DateTime gFrameEndTime = new();
+            DateTime gFrameStartTime = new();
+
+            // ball
+            Ball ball = new Ball(new Vec2(10, 10));
+            double ballVel = 100;
+            Vec2 ballDir = new Vec2(5, 2).NormalizeInPlace();
+            Vec2 ballNextPos = new Vec2();
+
+            // paddles
+            double paddleSpeed = 5;
+            double paddleBorderDistance = 10;
+            double paddleWidth = 4;
+            double paddleHeight = 10;
+
+            // paddleLeft
+            Rectangle paddleLeft = new Rectangle(new Vec2(), new Vec2(paddleWidth, paddleHeight), new Vec2(1, 1));
+
+
+            gFrameStartTime = DateTime.Now;
             while (true)
             {
                 frameStartTime = DateTime.Now;
+                gFrameEndTime = DateTime.Now;
 
                 // Frame counting
                 frameCounter++;
@@ -50,24 +77,35 @@ namespace ASCII_Render_Engine
                     Console.Title = $"FPS: {currentFPS:F2}"; // Update console title with FPS
                 }
 
-                if (screen.Config.ScaleToWindow)
+                frame.Size.SetInPlace(screen.Width, screen.Height);
+
+                deltaTime = (gFrameEndTime - gFrameStartTime).TotalSeconds;
+
+                ballNextPos.x = ball.Pos.x + ballDir.x * ballVel * deltaTime;
+                if (ballNextPos.x + ball.Radius > screen.Width || ballNextPos.x - ball.Radius < 0)
                 {
-                    screen.ScaleToWindow();
-                    frame.Size.SetInPlace(screen.Width, screen.Height);
+                    ballDir.x = -ballDir.x;
+                    ballNextPos.x = ball.Pos.x + ballDir.x * ballVel * deltaTime;
                 }
+
+                ballNextPos.y = ball.Pos.y + ballDir.y * ballVel * deltaTime;
+                if (ballNextPos.y + ball.Radius > screen.Height || ballNextPos.y - ball.Radius < 0)
+                {
+                    ballDir.y = -ballDir.y;
+                    ballNextPos.y = ball.Pos.y + ballDir.y * ballVel * deltaTime;
+                }
+
+                ball.Pos.SetInPlace(ballNextPos);
+                ball.Radius = 7;
 
                 screen.Clear();
 
-                // api-injected objects
-                foreach (ShapeData shape in ShapeStore.GetShapes())
-                {
-                    screen.Draw(shape.shape);
-                }
+                screen.Draw(ball.ToRenderable());
 
                 screen.Draw(frame);
 
                 // Render (fire-and-forget)
-                screen.RenderAsync();
+                screen.Render();
 
                 frameEndTime = DateTime.Now;
 
@@ -82,8 +120,8 @@ namespace ASCII_Render_Engine
                     // Sleep for the remaining time to maintain FPS
                     Thread.Sleep((int)(sleepTime * 1000)); // Convert seconds to milliseconds
                 }
+                gFrameStartTime = gFrameEndTime;
             }
         }
-
     }
 }
